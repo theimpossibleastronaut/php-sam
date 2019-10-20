ba59a583786058d269470750695c74a1e9d28dd7
Martijn de Boer <git@sexybiggetje.nl>
2019-10-20 12:55:14 +0200

    Update signatures

--- lib/Signatures.php
+++ lib/Signatures.php
@@ -7,7 +7,7 @@ class Signatures
 	const ECDSA_SHA256_P256 = "ECDSA_SHA256_P256";
 	const ECDSA_SHA384_P384 = "ECDSA_SHA384_P384";
 	const ECDSA_SHA512_P521 = "ECDSA_SHA512_P521";
-	const EdDSA_SHA512_Ed25519 = "EdDSA_SHA512_Ed25519";
+	const EDDSA_SHA512_ED25519 = "EdDSA_SHA512_Ed25519";
 
 	public static function getSignatureType( string $signatureType ):String
 	{
